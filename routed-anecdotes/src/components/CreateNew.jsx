392d565ce2d87a952294007181d88a2843f1f0e7
Fari-panah <panahfari81@gmail.com>
2026-09-19 09:41:22 +0300

    7.1: useField hook

--- routed-anecdotes/src/components/CreateNew.jsx
+++ routed-anecdotes/src/components/CreateNew.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react"
+import { useField } from "../hooks/index"
 import { useNavigate } from "react-router-dom"
 
 const CreateNew = ({ addNew }) => {
-  const [content, setContent] = useState("")
-  const [author, setAuthor] = useState("")
-  const [info, setInfo] = useState("")
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    addNew({ content, author, info, votes: 0 })
+    addNew({ 
+      content: content.value,
+      author: author.value,
+      info: info.value, 
+      votes: 0 })
     navigate("/")
   }
 
@@ -21,24 +25,21 @@ const CreateNew = ({ addNew }) => {
           content
           <input
             name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            {...content}
           />
         </div>
         <div>
           author
           <input
             name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
+            {...author}
           />
         </div>
         <div>
           url for more info
           <input
             name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
+            {...info}
           />
         </div>
         <button>create</button>
